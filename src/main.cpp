4fc082da8aa047361ffc78a5a8edbd5abec6d551
JamesBremner <james@ravenspoint.com>
2023-01-08 12:05:37 -0500

    update main for dfs_cycle_finder

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,21 @@ main()
 {
     // construct test graph
     cGraph graph;
-    graph.setEdges("a b\nb c\nc d\nd a\na x\nx y");
+    graph.setEdges("a b\nb c\nc d\nd a");
 
     // display graph edges
     std::cout << graph.text();
 
     // run the cycle detector
-    graph.dfs_cycle_finder("a");
+    auto vCycle = graph.dfs_cycle_finder("a");
+
+    for( auto& vv : vCycle )
+    {
+        std::cout << "cycle: ";
+        for( auto v : vv )
+            std::cout << v->userName() << " ";
+        std::cout << "\n";
+    }
 
     return 0;
 
